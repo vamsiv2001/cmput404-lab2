ede6b5845b624cccae1c6fc2ac19e7a56d8604ae
Vamsi Vasamsetti <56233789+vamsiv2001@users.noreply.github.com>
2023-09-27 16:59:39 -0600

    Update proxy_client.py

--- proxy_client.py
+++ proxy_client.py
@@ -3,7 +3,7 @@ import socket
 BYTES_TO_READ = 4096
 
 def get(host, port):
-    request = b"GET / HTTP/1.1\n Host: www.google.com\n\n"
+    request = b"GET / HTTP/1.1\nHost: www.google.com\n\n"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #AF_INET => IPv4, SOCK_STREAM => TCP
         s.connect((host, port))
         s.send(request)
